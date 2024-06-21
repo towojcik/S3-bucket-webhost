8142044e5b6b15dedda6789e0f91a8b7c01dccc3
Tomasz Wójcik <towojcik@gmail.com>
2024-06-21 13:17:13 +0200

    Change provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,16 +2,12 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.39.0"
+      version = "5.55.0"
     }
   }
 }
 
 provider "aws" {
-<<<<<<< HEAD
-  region = "eu-east-1"
-}
-=======
   region = "eu-central-1"
+
 }
->>>>>>> 934d16a (Add Jenkinsfile)
